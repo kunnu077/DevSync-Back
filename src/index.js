0a9a4026f215643ee238f6ffb758358eaea0bd58
kunnu077 <146961274+kunnu077@users.noreply.github.com>
2025-12-25 20:57:44 +0530

    database intialized

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 const express = require('express');
 const app = express();
 const connectDB = require('./config/database');
+const User = require('./models/user');
+
+app.post("/signup", async(req, res)=>{
+
+//creating instance of the user model
+const user = new User({
+    firstName: "Kunal",
+    lastName: "Kushwah",
+    email: "kunal@example.com",
+    password: "password123",
+    age: 25,
+    gender: "Male"
+});
+
+await user.save();
+res.send("User signed up successfully");
+
+});
 
 connectDB().then(()=>{
     console.log("Database connected successfully");
